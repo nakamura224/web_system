e96bf44e992b7fc5d79300bcddf48440cb06acec
Nakamura Sana <dq.tri.fi@gmail.com>
2022-01-21 12:24:44 +0900

    変更しました

--- public/login.php
+++ public/login.php
@@ -6,7 +6,7 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
   // POSTで email と password が送られてきた場合のみログイン処理をする
 
   // email から会員情報を引く
-  $select_sth = $dbh->prepare("SELECT * FROM koki02_users WHERE email = :email ORDER BY id DESC LIMIT 1");
+  $select_sth = $dbh->prepare("SELECT * FROM users WHERE email = :email ORDER BY id DESC LIMIT 1");  
   $select_sth->execute([
     ':email' => $_POST['email'],
   ]);
@@ -18,53 +18,21 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
     header("Location: ./login.php?error=1");
     return;
   }
-   // パスワードが正しいかチェック
-  $password_hash = mb_substr($user['password'], 0, 64); // 0文字目から64文字分がハッシュ
-  $salt = mb_substr($user['password'], 64, 64); // 64文字目から64文字分がソルト
-  
-  $input_password_hash = $_POST['password'];
-  foreach (range(1, 100) as $count) {
-    // ソルトを追加してハッシュ化... を100回繰り返す
-    $input_password_hash = hash('sha256', $input_password_hash . $salt);
-  }
-
-  $correct_password = $input_password_hash === $password_hash;
-
-
+   
+  $correct_password = password_verify($_POST['password'], $user['password']);
    if (!$correct_password) {
      // パスワードが間違っていれば、処理を中断しエラー用クエリパラメータ付きのログイン画面URLにリダイレクト
      header("HTTP/1.1 302 Found");
      header("Location: ./login.php?error=1");
      return;
    }
- 
- 
-   # ここからセッションの独自実装 (詳細は後期第1回授業参照) ##############
-   // セッションIDの取得(なければ新規で作成&設定)
-   $session_cookie_name = 'session_id';
-   $session_id = $_COOKIE[$session_cookie_name] ?? base64_encode(random_bytes(64));
-   if (!isset($_COOKIE[$session_cookie_name])) {
-     setcookie($session_cookie_name, $session_id);
-   }
-    // 接続 (redisコンテナの6379番ポートに接続)
-  $redis = new Redis();
-  $redis->connect('redis', 6379);
 
-  // Redisにセッション変数を保存しておくキー
-  $redis_session_key = "session-" . $session_id; 
-
-  // Redisからセッションのデータを読み込み
-  // 既にセッション変数(の配列)が何かしら格納されていればそれを，なければ空の配列を $session_values変数に保存
-  $session_values = $redis->exists($redis_session_key)
-    ? json_decode($redis->get($redis_session_key), true) 
-    : []; 
-
+   session_start();
+   
   // セッションにログインできた会員情報の主キー(id)を設定
-  $session_values["login_user_id"] = $user['id'];
+  $_SESSION["login_user_id"] = $user['id'];
 
-  // セッションをRedisに保存
-  $redis->set($redis_session_key, json_encode($session_values));
-  # セッションここまで ###################################################
+  
 // ログインが成功したらログイン完了画面にリダイレクト
 header("HTTP/1.1 302 Found");
 header("Location: ./login_finish.php");
@@ -73,6 +41,8 @@ return;
 ?>
 
 <h1>ログイン</h1>
+初めての人は<a href="/signup.php">会員登録</a>しましょう。
+<hr>
 
 <!-- ログインフォーム -->
 <form method="POST">
--- public/signup.php
+++ public/signup.php
@@ -5,31 +5,25 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password
   // POSTで name と email と password が送られてきた場合はDBへの登録処理をする
 
   // 既に同じメールアドレスで登録された会員が存在しないか確認する
-  $insert_sth = $dbh->prepare("SELECT * FROM koki02_users WHERE email = :email ORDER BY id DESC LIMIT 1");
-  $insert_sth->execute([
+  $select_sth = $dbh->prepare("SELECT * FROM users WHERE email = :email ORDER BY id DESC LIMIT 1");
+  $select_sth->execute([
     ':email' => $_POST['email'],
   ]);
-  $user = $insert_sth->fetch();
+  $user = $select_sth->fetch();
   if (!empty($user)) {
     // 存在した場合 エラー用のクエリパラメータ付き会員登録画面にリダイレクトする
     header("HTTP/1.1 302 Found");
     header("Location: ./signup.php?duplicate_email=1");
     return;
   }
-  //ソルトを決める
-  $salt = bin2hex(random_bytes(32));
-  $password_hash = $_POST['password'];
-  foreach (range(1, 100) as $count) {
-    // ソルトを追加してハッシュ化... を100回繰り返す
-    $password_hash = hash('sha256', $password_hash . $salt);
-  }
+  
 
   // insertする
-  $insert_sth = $dbh->prepare("INSERT INTO koki02_users (name, email, password) VALUES (:name, :email, :password)");
+  $insert_sth = $dbh->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
   $insert_sth->execute([
       ':name' => $_POST['name'],
       ':email' => $_POST['email'],
-      ':password' => $password_hash . $salt,
+      ':password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
   ]); 
 
   // 処理が終わったら完了画面にリダイレクト
@@ -40,6 +34,10 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password
 ?>
 
 <h1>会員登録</h1>
+
+会員登録済の人は<a href="/login.php">ログイン</a>しましょう。
+<hr>
+
 <!-- 登録フォーム -->
 <form method="POST">
   <!-- input要素のtype属性は全部textでも動くが、適切なものに設定すると利用者は使いやすい -->
